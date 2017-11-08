2ece9e490c94caf941bd5e0472bf2b470730c851
Your Name <resorttoken@gmail.com>
2017-11-07 22:40:52 -0800

    add Landing page and dashboard___

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.0"
 
 set :application, 'resorttoken'
-set :repo_url, 'git@github.com:resorttoken/resorttoken' # Edit this to match your repository
+set :repo_url, 'https://github.com/resorttoken/resorttoken' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/resorttoken'
 set :pty, true
