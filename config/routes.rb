539ffe4ba842a17af9473ef06a44c2ec6428741f
resorttoken@gmail.com <resorttoken@gmail.com>
2017-11-17 10:16:19 -0800

    implement ssl

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
+
+  # Home Page
   get 'home/index'
   get 'home/minor'
+
+  # Landing Page
   get 'landing/index'
+
+  # Dashboard Page
+  get 'dashboards/dashboard_main'
+
   #root :to => "home#index"
   root :to => "landing#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
