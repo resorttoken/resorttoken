bd30e2d93e8cd4c1b58af9c6ba287d68f5546c5a
resorttoken@gmail.com <resorttoken@gmail.com>
2017-12-07 23:23:52 -0800

    fix db migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -15,27 +15,8 @@ ActiveRecord::Schema.define(version: 20171205072242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "customers", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "reference"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
